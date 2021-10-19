c3f07f67f5ed28a12395b3f63f8e5c34dc276feb
Tomek <tomek.uzarowski@gmail.com>
2021-10-19 22:36:59 +0200

    PHP fix: got rid of unnecessary time() inside getdate()

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ require "vendor/autoload.php";
 
 use Calendar\Calendar;
 
-$now = getdate(time());
+$now = getdate();
 $calendar = new Calendar($now["mon"], $now["year"]);
 echo $calendar->getTable();
